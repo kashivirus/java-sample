f290a9052b275e0da05f747b09408ea54aba8915
kashivirus <kashivirus691@gmail.com>
2022-12-15 15:04:27 +0500

    s

--- MavenProjectDsl.groovy
+++ MavenProjectDsl.groovy
@@ -19,25 +19,4 @@ job('First-Maven-Project-Via-DSL') {
 
 
 
-
-
-// jon("First-Maven-Project-Via-DSL"){
-//     description("First Maven Job gennerated  by DSL on ${new Date()}, this is simple maven project")
-
-
-//     scm{
-//         git("https://github.com/kashivirus/java-sample.git" , 'main')
-//     }
-
-//     triggers{
-//         scm("* * * * *")
-//     }
-
-//     steps{
-//         maven("clean package", 'maven-samples/single-module/pom.xml')
-//     }
-
-//     publishers{
-//         archiveArtifacts "**/*.jar"
-//     }
-// }
+ 
